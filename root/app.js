88252303ac693f9fd46ec526d386ca4c00e43235
Jeff Escalante <escalantejeff@gmail.com>
2017-02-23 16:48:39 -0500

    upgrades for spike@next

--- root/app.js
+++ root/app.js
@@ -1,19 +1,14 @@
 const htmlStandards = require('spike-html-standards')
 const cssStandards = require('spike-css-standards')
-const latest = require('babel-preset-latest')
+const jsStandards = require('babel-preset-env')
 
 module.exports = {
   devtool: 'source-map',
-  ignore: ['**/layout.html', '**/_*', '**/.*'],
-  reshape: (ctx) => {
-    return htmlStandards({
-      parser: false,
-      webpack: ctx,
-      locals: { foo: 'bar' }
-    })
-  },
-  postcss: (ctx) => {
-    return cssStandards({ parser: false, webpack: ctx })
-  },
-  babel: { presets: [latest] }
+  ignore: ['**/layout.html', '**/_*', '**/.*', 'readme.md', 'yarn.lock'],
+  reshape: htmlStandards({
+    parser: false,
+    locals: { foo: 'bar' }
+  }),
+  postcss: cssStandards({ parser: false }),
+  babel: { presets: [[jsStandards, { modules: false }]] }
 }
--- root/app.production.js
+++ root/app.production.js
@@ -1,34 +1,18 @@
 const htmlStandards = require('spike-html-standards')
 const cssStandards = require('spike-css-standards')
-const webpack = require('webpack')
-const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin
-const DedupePlugin = webpack.optimize.DedupePlugin
-const OccurrenceOrderPlugin = webpack.optimize.OccurrenceOrderPlugin
+const {UglifyJsPlugin} = require('webpack').optimize
 
 module.exports = {
   // disable source maps
   devtool: false,
   // webpack optimization and minfication plugins
-  plugins: [
-    new UglifyJsPlugin(),
-    new DedupePlugin(),
-    new OccurrenceOrderPlugin()
-  ],
-  // image optimization
-  module: {
-    loaders: [{ test: /\.(jpe?g|png|gif|svg)$/i, loader: 'image-webpack' }]
-  },
+  plugins: [new UglifyJsPlugin()],
   // adds html minification plugin
-  reshape: (ctx) => {
-    return htmlStandards({
-      parser: false,
-      webpack: ctx,
-      locals: { foo: 'bar' },
-      minify: true
-    })
-  },
+  reshape: htmlStandards({
+    parser: false,
+    locals: { foo: 'bar' },
+    minify: true
+  }),
   // adds css minification plugin
-  postcss: (ctx) => {
-    return cssStandards({ parser: false, webpack: ctx, minify: true })
-  }
+  postcss: cssStandards({ parser: false, minify: true })
 }
